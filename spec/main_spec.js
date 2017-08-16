27ecc151fa94f3e0066eb9ffbe3579d0a6f604d0
29984608 <29984608@qq.com>
2017-08-16 18:57:36 +0800

    测试完成

---
+++ spec/main_spec.js
@@ -0,0 +1,53 @@
+"use strict";
+var _ = require("lodash");
+var chai = require("chai");
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var expect = chai.expect;
+chai.use(sinonChai);
+
+var main = require("../lib/main.js");
+
+
+describe("测试描述", function(){
+    sinon.spy(console, 'log');
+
+    it("测试用例1:(95713)", function(){
+
+        var result = main('95713');
+        var expect_string = '|	|:|::	:|:|:	|:::|	:::||	::||:	:|:|:	|';
+        
+        expect(expect_string).to.equal(result);
+    });
+
+    it("测试用例2:(95713-9571)", function(){
+
+        var result = main('95713-9571');
+        var expect_string = '|	|:|::	:|:|:	|:::|	:::||	::||:	|:|::	:|:|:	|:::|	:::||	::||:	|';
+
+        expect(expect_string).to.equal(result);
+    });
+
+    it("测试用例3:(95718)", function(){
+
+        var result = main('95718');
+        var expect_string = '|	|:|::	:|:|:	|:::|	:::||	|::|:	||:::	|';
+
+        expect(expect_string).to.equal(result);
+    });
+    it("测试用例4:('|	|:|::	:|:|:	|:::|	:::||	::||:	:|:|:	|')", function(){
+
+        var result = main('|	|:|::	:|:|:	|:::|	:::||	::||:	:|:|:	|');
+        var expect_string = '95713';
+
+        expect(expect_string).to.equal(result);
+    });
+
+    it("测试用例5:('|	|:|::	:|:|:	|:::|	:::||	::||:	|:|::	:|:|:	|:::|	:::||	::||:	|')", function(){
+
+        var result = main('|	|:|::	:|:|:	|:::|	:::||	::||:	|:|::	:|:|:	|:::|	:::||	::||:	|');
+        var expect_string = '95713-9571';
+
+        expect(expect_string).to.equal(result);
+    });
+});
